26370719562dfebd889ad8a0602f118dd9a40d10
Abhay-Tewatia <abhaytewtia342@gmail.com>
2025-11-26 13:18:39 +0530

    how to install and add and use bootstarp with the main.jsx file or app.jsx file

---
+++ 17 Bootstrap/learning-bootstrap/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return  <div className="card" style={{width: "18rem"}}>
+  <img className="card-img-top" src="https://imgs.search.brave.com/G-bp9Iu-qwlZtR2Idn9a5ZHRpM6LzhxWi8LUTrmQmnQ/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdGVh/ZHlmcmVkZHkuY29t/L2Nkbi9zaG9wL2Fy/dGljbGVzL2RvZ2d5/LXN0eWxlLXBvc2l0/aW9uLmpwZz92PTE2/ODM5NjQyMTImd2lk/dGg9MTQwMA" alt="Card image cap" />
+  <div className="card-body">
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+    </div>
+  
+}
+
+export default App
